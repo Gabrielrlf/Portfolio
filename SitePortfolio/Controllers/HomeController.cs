57fa967c2df19096b0e4ea57f80c66c48b874552
Gabriel Fonseca <Biellimaet@gmail.com>
2020-06-14 04:15:12 -0300

    Iniciando o site

---
+++ SitePortfolio/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitePortfolio.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Sobre mim";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Meus contatos";
+
+			return View();
+		}
+		
+		public ActionResult Portfolio()
+		{
+			ViewBag.Message = "Veja alguns dos meus trabalhos";
+			
+			return View();
+		}
+	}
+}
