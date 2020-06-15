a70c080f9659c2448ccee4bc0a883046d18eb534
Gabriel Fonseca <Biellimaet@gmail.com>
2020-06-15 18:47:24 -0300

    Ajustes e implementado caixa de search com função de datalist autoimplementada através de js

---
+++ SitePortfolio/Scripts/Javascript/JavaScript.js
@@ -0,0 +1,18 @@
+﻿function execution() {
+    var texto = document.getElementById('texto').value;
+    var list = document.getElementById('completar');
+    var adicionar = true;
+
+    var opcoes = document.createElement('option');
+
+    for (i = 0, i < list.opcoes.length; i++) {
+        if (texto == list.opcoes[i]) {
+            adicionar = false;
+        }
+    }
+
+    if (adicionar == true) {
+        opcoes.value == texto;
+        list.appendChild(opcoes);
+    }
+}
